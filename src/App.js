4e096ac878b82b3fee47e66532bdb60210ca32fb
ej-yotpo <161460939+ej-yotpo@users.noreply.github.com>
2025-02-12 22:20:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "513953";
-  let InstanceId_QA = "513951";
-  let InstanceId_SEO = "600662";
-  let InstanceId_Carousel = "600665";
-  let InstanceId_StarRating = "491372";
-  let InstanceId_PromotedProd = "608401";
+  let InstanceId_Widget = "1032782";
+  let InstanceId_QA = "1032880";
+  let InstanceId_SEO = "";
+  let InstanceId_Carousel = "";
+  let InstanceId_StarRating = "1032868";
+  let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
